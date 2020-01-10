de2d7a97b5b5daab10636c2e7c560f6889cc9d78
Maureen Kariuki <mkariuki2@mail.greenriver.edu>
2020-01-10 13:23:09 -0500

    Home page root added

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+//This is our Controller!
+//Turn on error reporting -- this is critical
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+include_once('vendor/autoload.php');
+
+//Create an instance of the base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run fat free
+$f3->run();
